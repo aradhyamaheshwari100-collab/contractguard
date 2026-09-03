343da9051640901350008f95b9f8177bd6123fbb
aradhyamaheshwari100-collab <aradhyamaheshwari100@gmail.com>
2026-09-04 00:35:43 +0530

    refactor: remove backend configuration and dependencies

--- backend/tools/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Tools subpackage."""
-
-from backend.tools.scam_lists import search_known_scam_lists, load_scam_database
-
-__all__ = ["search_known_scam_lists", "load_scam_database"]
--- config.py
+++
@@ -1,5 +0,0 @@
-"""Root config module re-exporting backend.config.settings."""
-
-from backend.config import settings, Settings
-
-__all__ = ["settings", "Settings"]
